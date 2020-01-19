51ec02d25db7c12cf5f664d78080799111ae3ec4
ranveerverma709@gmail.com <ranveerverma709@gmail.com>
2020-01-19 21:04:38 +0530

    Added Reset Button

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,18 +1,7 @@
 import React, { Component } from "react";
 export class Item extends Component {
-  state = {
-    count: this.props.value
-  };
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  decrement = () => {
-    if (this.state.count > 0) {
-      this.setState({ count: this.state.count - 1 });
-    }
-  };
   getBadgeColor() {
-    return this.state.count === 0 ? "warning" : "primary";
+    return this.props.item.count === 0 ? "warning" : "primary";
   }
 
   render() {
@@ -22,11 +11,11 @@ export class Item extends Component {
           className={`badge-${this.getBadgeColor()} badge badge-lg mr-2 align-middle p-3`}
           style={{ fontSize: "24px" }}
         >
-          {this.state.count}
+          {this.props.item.count}
         </span>
         :
         <button
-          onClick={this.increment}
+          onClick={() => this.props.onIncrement(this.props.item)}
           className="btn btn-secondary btn-lg ml-2 mr-3"
           id="inc"
         >
@@ -41,7 +30,7 @@ export class Item extends Component {
         </button>
         <button
           className="btn btn-danger"
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.item.id)}
         >
           delete
         </button>
--- src/components/layouts/Counter.js
+++ src/components/layouts/Counter.js
@@ -6,6 +6,7 @@ export class Counter extends Component {
     i: 0,
     items: []
   };
+
   itemIncrement = () => {
     this.setState({ i: this.state.i + 1 });
     let obj = { id: this.state.i, count: 0 };
@@ -13,24 +14,45 @@ export class Counter extends Component {
       items: [...this.state.items, obj]
     });
   };
+  handleIncrement = counter => {
+    console.log(counter.id);
+    let items = [...this.state.items];
+    let index = items.indexOf(counter);
+    items[index] = { ...counter };
+    items[index].count++;
+    this.setState({ items });
+  };
   handleDelete = itemId => {
     console.log("handled event");
     const items = this.state.items.filter(c => c.id !== itemId);
-    this.setState({ i: this.state.i - 1, items: items });
+    this.setState({ items: items });
+  };
+  handleReset = () => {
+    console.log("reset");
+    let items = this.state.items.map(item => {
+      item.count = 0;
+      return item;
+    });
+    this.setState({ items });
   };
   render() {
     return (
       <div>
         <Navbar value={this.state.i} />
-        <button className="btn btn-dark ml-4 mt-4" onClick={this.itemIncrement}>
-          Add Item +
-        </button>
+        <div className=" ml-4 mt-4">
+          <button className="btn btn-dark mr-4" onClick={this.itemIncrement}>
+            Add Item +
+          </button>
+          <button className="btn btn-dark" onClick={this.handleReset}>
+            Reset
+          </button>
+        </div>
         {this.state.items.map(item => (
           <Item
             key={item.id}
             onDelete={this.handleDelete}
-            id={item.id}
-            value={item.count}
+            onIncrement={this.handleIncrement}
+            item={item}
           />
         ))}
       </div>
